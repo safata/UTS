80c7d0c465f345b6780e6c896973255a7eb75993
safata <annisasafataa@gmail.com>
2020-03-28 10:31:06 +0700

    Menambahkan onCreateView, onAttach pada HomeFragment

--- app/src/main/java/com/example/utscoba/fragments/HomeFragment.java
+++ app/src/main/java/com/example/utscoba/fragments/HomeFragment.java
@@ -1,10 +1,65 @@
 package com.example.utscoba.fragments;
 
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import androidx.fragment.app.Fragment;
+
+import com.example.utscoba.R;
 
 public class HomeFragment extends Fragment {
 
+    private OnFragmentInteractionListener mListener;
+
     public HomeFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false );
+
+        ImageView jajan = view.findViewById(R.id.jajan);
+
+
+        jajan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mListener != null){
+                    mListener.onMeatClicked();
+                }
+            }
+        });
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onMeatClicked();
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onMeatClicked();
     }
 }
